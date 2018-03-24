2ba7d0fd663f010079441724f8845214d1054d57
Aldo Torres <atorres6d5@gmail.com>
2018-03-23 23:51:36 -0700

    added api request responce

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Kids from './components/kids.js'
-import logo from './logo.svg';
 import './App.css';
 import ParentView from "./components/parentView.js"
 
@@ -8,7 +7,17 @@ import ParentView from "./components/parentView.js"
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      "user":{}
+    }
+  }
 
+  setLocalStorage = () =>{
+    localStorage.setItem("client_id", "vgw3sf4f8nq3b98i1gdfr8wpx4gpty0ska52")
+    localStorage.setItem("client_secret", "eb5f6rda6v0d1ld8y4fymkudo86gorrc47cj")
+  }
 
   render() {
     return (
--- src/components/parentView.js
+++ src/components/parentView.js
@@ -12,17 +12,6 @@ class ParentView extends Component {
 
 
 
-
-  getToken = () => {
-    axios.post("https://api.dxhackathon.com/oauth2/token", {
-      client_id:"vgw3sf4f8nq3b98i1gdfr8wpx4gpty0ska52",
-      client_secret:"eb5f6rda6v0d1ld8y4fymkudo86gorrc47cj",
-      grant_type:"client_credentials"
-    })
-  }
-
-  
-
   render() {
     console.log(this.getToken())
     return (
